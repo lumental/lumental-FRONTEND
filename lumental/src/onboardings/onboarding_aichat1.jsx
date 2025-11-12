9af96bfc1a67c91879596a27636b770b9addc592
GirimNam <rlfla0721@naver.com>
2025-11-12 18:13:34 +0900

    온보딩2

---
+++ lumental/src/onboardings/onboarding_aichat1.jsx
@@ -0,0 +1,14 @@
+import aichat from '../assets/온보딩_챗봇.png';
+
+export default function OB_aichat() {
+  return (
+    <main style={{ maxWidth: 430, margin: '0 auto', padding: '16px 16px 88px', borderLeft: '1px solid rgba(0,0,0,0.08)',
+    borderRight: '1px solid rgba(0,0,0,0.08)',  }}>
+    
+        
+        <img src={aichat} alt="온보딩_챗봇" />
+
+      
+    </main>
+  );
+}
