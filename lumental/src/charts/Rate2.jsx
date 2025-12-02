fd88bfadedfd9d579aa568cb81c1931f8f9e624a
GirimNam <rlfla0721@naver.com>
2025-12-03 01:06:04 +0900

    그래프 수정9

--- lumental/src/charts/Rate2.jsx
+++ lumental/src/charts/Rate2.jsx
@@ -1,4 +1,4 @@
-import { Line, LineChart } from 'recharts';
+import { Line, LineChart, CartesianGrid, YAxis } from 'recharts';
 
 
 
@@ -10,7 +10,21 @@ export default function Rate({data}) {
       responsive
       data={data}
     >
-      <Line type="monotone" dataKey="pv" stroke="#8884d8" strokeWidth={2} />
+      <CartesianGrid
+        stroke="#ccc"
+        strokeDasharray="0"
+        vertical={false}
+      />
+
+      {/* ✔ 사진처럼 Y축 숫자(50, 80, 110, 140 …) 표시 */}
+      <YAxis
+        domain={['dataMin - 5', 'dataMax + 5']}
+        tick={{ fontSize: 10, fill: '#999' }}
+        axisLine={false}
+        tickLine={false}
+      />
+
+      <Line type="monotone" dataKey="value" stroke="#8884d8" strokeWidth={2} />
     </LineChart>
   );
 }
