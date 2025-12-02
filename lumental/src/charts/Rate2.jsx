33bc469dc410c51b2d0131aaac6ec61e4171c31c
GirimNam <rlfla0721@naver.com>
2025-12-03 00:52:52 +0900

    그래프 수정8

--- lumental/src/charts/Rate2.jsx
+++ lumental/src/charts/Rate2.jsx
@@ -1,27 +1,16 @@
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { Line, LineChart } from 'recharts';
 
 
 
+// #endregion
 export default function Rate({data}) {
   return (
     <LineChart
-      style={{ width: '90%', maxWidth: '700px', height: '90%', maxHeight: '70vh', aspectRatio: 1.618 }}
+      style={{ width: '100%', maxWidth: '300px', maxHeight: '100px', aspectRatio: 1.618 }}
       responsive
       data={data}
-      margin={{
-        top: 5,
-        right: 0,
-        left: 0,
-        bottom: 5,
-      }}
     >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis width="auto" />
-      <Tooltip />
-      <Legend />
-      <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-      
+      <Line type="monotone" dataKey="pv" stroke="#8884d8" strokeWidth={2} />
     </LineChart>
   );
 }
